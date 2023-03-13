734e5e62a7efe4b2dc848147915189b0d4d4561d
Matthewmeadows4 <101786489+Matthewmeadows4@users.noreply.github.com>
2023-03-12 21:55:07 -0400

    Committed Changes

---
+++ EmptyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KnowCheck;
+
+
+public class Book
+{
+    public int pageNumber { get; set; }
+
+    public Book()
+    {
+        this.pageNumber = 0;
+
+    }
+}
+
+public class Novel : Book
+{
+    public Novel()
+    {
+        this.pageNumber = 0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace KnowCheck;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var LordOfTheRings = new Book();
+        Console.WriteLine("How many records do you want to add? ");
+        var numberOfRecords = int.Parse(Console.ReadLine());
+
+        var recordList = new List<Book>();
+        for (int i = 0; i < numberOfRecords; i++)
+        {
+            // In this loop, populate the object's properties using Console.ReadLine()
+            var myBook= new Book();
+
+            Console.WriteLine("Enter the value for ");
+            myBook.pageNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            recordList.Add(myBook);
+            Console.WriteLine("Number of pages: " + myBook.pageNumber);
+        }
+    }
+}
